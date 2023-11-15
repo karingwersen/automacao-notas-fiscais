8987fa6aeef303a396e1161b47398fdf3a3752ff
karingwersen <karin.ingwersen96@gmail.com>
2023-11-15 19:06:06 -0300

    Leitura da planilha(arquivo xlsx)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,13 @@
-
+import openpyxl
 
 if __name__ == '__main__':
-    pass
+
+    path = "C:\\Users\\karin\\Downloads\\PLANILHA NF'S 2018.xlsx"
+    planilha = openpyxl.load_workbook(path)
+    sheet = planilha.active
+
+    max_row = sheet.max_row
+    for i in range(1, max_row + 1):
+        cell = sheet.cell(row = i, column = 1)
+        print(cell.value)
+
